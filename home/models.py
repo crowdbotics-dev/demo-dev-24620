2412d2312a4d39a6f435e0b18519ccb49a5db2b2
Crowdbotics <engineering@crowdbotics.com>
2022-06-03 07:08:12 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -5,3 +5,11 @@ from django.db import models
 class XYz(models.Model):
     "Generated Model"
     name = models.BigIntegerField()
+    user = models.TextField(
+        null=True,
+        blank=True,
+    )
+    address = models.TextField(
+        null=True,
+        blank=True,
+    )
